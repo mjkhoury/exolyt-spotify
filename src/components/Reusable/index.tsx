3d82adfe2d9dd4edaeee4f9259c91ffa73dcf70c
mjkhoury <mjkhoury14@hotmail.com>
2023-02-01 00:17:44 +0200

    fixed Reusable Error component margin

--- src/components/Reusable/index.tsx
+++ src/components/Reusable/index.tsx
@@ -17,7 +17,7 @@ export const Title = styled.h1<TitleProps>`
 `
 
 export const Error = styled(Result)`
-  margin-top: ${({ theme }) => theme.margins.double}px;
+  margin-top: 78px;
   width: max-content;
   .ant-result-title,
   .ant-result-subtitle {
