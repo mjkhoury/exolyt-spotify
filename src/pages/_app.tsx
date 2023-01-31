f1cecca29cdd0a9269e180174a2791bfa87fe1ae
mjkhoury <mjkhoury14@hotmail.com>
2023-01-31 16:37:15 +0200

    Added antd theme and emotion theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,26 @@
 import '@Styles/globals.css'
 
 import Context from '@Context'
+import { Theme, ThemeProvider } from '@emotion/react'
+import COLORS from '@Static/colors'
+import AppTheme from '@Theme/AppTheme'
+import { ConfigProvider } from 'antd'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Context>
-      <Component {...pageProps} />
-    </Context>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: COLORS.green
+        }
+      }}
+    >
+      <ThemeProvider theme={AppTheme as Partial<Theme>}>
+        <Context>
+          <Component {...pageProps} />
+        </Context>
+      </ThemeProvider>
+    </ConfigProvider>
   )
 }
