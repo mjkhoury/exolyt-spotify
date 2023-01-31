9f028abba1e8b09703ac19b5f3999e060a24c841
mjkhoury <mjkhoury14@hotmail.com>
2023-01-31 16:13:17 +0200

    Added color palette

---
+++ src/static/colors.ts
@@ -0,0 +1,11 @@
+const COLORS = Object.freeze({
+  black: '#191414',
+  transparentBlack: 'rgba(25, 20, 20, 0.95)',
+  darkBlack: '#000000',
+  green: '#1DB954',
+  white: '#FFFFFF',
+  grey1: '#b3b3b3',
+  grey2: '#EEEEEE'
+})
+
+export default COLORS
