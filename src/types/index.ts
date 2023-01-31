78a2f200fb8d53638f02ea5218be17fc2de0b7a7
mjkhoury <mjkhoury14@hotmail.com>
2023-01-31 16:46:43 +0200

    Added TabOptions and MenuItem types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,10 @@
 export type GenreType = string
 
 export type GenresType = GenreType[]
+
+export type TabOptions = 'home' | 'genres'
+
+export type MenuItem = {
+  key: React.Key
+  keyPath?: React.Key[]
+}
