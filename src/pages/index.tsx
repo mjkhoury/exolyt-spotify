c2f58862e242e2d5c949b0f06f8777bf9f529e3c
mjkhoury <mjkhoury14@hotmail.com>
2023-01-31 16:11:23 +0200

    moved pages back to src

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import styles from '@Styles/Home.module.css'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Exolyt Spotify</title>
+        <meta name="description" content="Exolyt Spotify" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}></main>
+    </>
+  )
+}
