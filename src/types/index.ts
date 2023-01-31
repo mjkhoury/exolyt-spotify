85363878f11c616ae1e10093bbc863dbe7313d65
mjkhoury <mjkhoury14@hotmail.com>
2023-01-31 17:05:59 +0200

    Added TrackType and its children types

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,3 +13,46 @@ export type SelectedTabType = {
   tab: TabOptions
   subTab: string
 }
+
+export type TrackType = {
+  id: string
+  name: string
+  artists: ArtistType[]
+  album: AlbumType
+  duration_ms: number
+  explicit: boolean
+  preview_url: string
+  uri: string
+  popularity: number
+  disc_number: number
+  track_number: number
+  is_local: boolean
+  available_markets: string[]
+  external_urls: {
+    spotify: string
+  }
+}
+
+export type TracksType = TrackType[]
+
+export type ArtistType = {
+  id: string
+  name: string
+  uri: string
+}
+
+export type ArtistsType = ArtistType[]
+
+export type AlbumType = {
+  id: string
+  name: string
+  images: ImageType[]
+  uri: string
+  release_date: string
+}
+
+export type ImageType = {
+  height: number
+  width: number
+  url: string
+}
