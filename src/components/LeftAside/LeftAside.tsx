c56a5ebf740ae0c5ca161afd72eee4fc44b1207e
mjkhoury <mjkhoury14@hotmail.com>
2023-01-31 16:44:41 +0200

    installed framer-motion for animation
    Added Logo to leftAside

--- src/components/LeftAside/LeftAside.tsx
+++ src/components/LeftAside/LeftAside.tsx
@@ -1,6 +1,9 @@
+import SpotifyTextSVG from '@Static/SVG/SpotifyTextSVG'
 import AppTheme from '@Theme/AppTheme'
 import { Layout } from 'antd'
+import { motion } from 'framer-motion'
 
+const LOGO_WIDTH = 130
 export const ASIDE_WIDTH = 300
 
 const asideStyle: React.CSSProperties = {
@@ -9,10 +12,37 @@ const asideStyle: React.CSSProperties = {
   backgroundColor: AppTheme.colors.black
 }
 
+const logoStyle: React.CSSProperties = {
+  marginTop: AppTheme.margins.base,
+  width: LOGO_WIDTH
+}
+
 export default function LeftAside() {
   return (
     <Layout.Sider style={asideStyle} width={300}>
-      Aside
+      <motion.div
+        style={logoStyle}
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.2,
+            opacity: 0
+          },
+          visible: {
+            opacity: 1,
+            scale: 1,
+            transition: {
+              duration: 0.5,
+              delay: 1,
+              type: 'spring'
+            }
+          }
+        }}
+      >
+        <SpotifyTextSVG />
+        NavMenu
+      </motion.div>
     </Layout.Sider>
   )
 }
