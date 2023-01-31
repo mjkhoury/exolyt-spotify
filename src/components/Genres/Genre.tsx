000f5d44bb6141c53c7fcec271b14a5523b9a683
mjkhoury <mjkhoury14@hotmail.com>
2023-01-31 17:18:18 +0200

    Added Genre component to be used for a single Genre

---
+++ src/components/Genres/Genre.tsx
@@ -0,0 +1,47 @@
+import { Error } from '@Components/Reusable'
+import styled from '@emotion/styled'
+import { useGenre } from '@Hooks/useGenre'
+import { TrackType } from '@Types'
+import { Spin } from 'antd'
+
+interface GenreProps {
+  genre: string
+}
+
+const Genre = ({ genre }: GenreProps) => {
+  const { genreTracks, isLoading, isError } = useGenre(genre)
+
+  if (isLoading) return <StyledSpin size="large" />
+  if (isError)
+    return (
+      <Error
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+      />
+    )
+
+  return (
+    <>
+      <TracksWrapper>
+        {genreTracks.map((track: TrackType) => {
+          if (!track?.name) return null
+          return <div key={track.id}>{track.name}</div>
+        })}
+      </TracksWrapper>
+    </>
+  )
+}
+
+export default Genre
+
+const TracksWrapper = styled.div`
+  height: 80vh;
+  padding-top: ${({ theme }) => theme.paddings.double};
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
+  grid-gap: 2rem;
+`
+const StyledSpin = styled(Spin)`
+  margin-top: 2rem;
+`
