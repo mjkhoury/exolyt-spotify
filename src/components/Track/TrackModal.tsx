0289fc65a260b17c339efab5080c0a632e233f97
mjkhoury <mjkhoury14@hotmail.com>
2023-01-31 17:32:04 +0200

    Created a track modal

---
+++ src/components/Track/TrackModal.tsx
@@ -0,0 +1,44 @@
+import { Spotify_data } from '@Context'
+import styled from '@emotion/styled'
+import { Modal } from 'antd'
+import { useContext } from 'react'
+
+interface TrackModalProps {
+  visible: boolean
+  onCancel: () => void
+}
+
+const TrackModal = ({ visible, onCancel }: TrackModalProps) => {
+  const { selectedTrack } = useContext(Spotify_data)
+  return (
+    <StyledModal
+      visible={visible}
+      onCancel={onCancel}
+      footer={null}
+      width={1000}
+      destroyOnClose
+    >
+      {selectedTrack.name}
+  </StyledModal>
+  )
+}
+
+export default TrackModal
+
+const StyledModal = styled(Modal)`
+  .ant-modal-content {
+    background-color: ${({ theme }) => theme.colors.transparentPrimary};
+    border: 1px solid ${({ theme }) => theme.colors.white};
+    box-shadow: 0 0 5px ${({ theme }) => theme.colors.white};
+  }
+  .ant-modal-title {
+    color: ${({ theme }) => theme.colors.primary};
+    background: transparent !important;
+  }
+  .ant-modal-close-x {
+    color: ${({ theme }) => theme.colors.white};
+    :hover {
+      color: ${({ theme }) => theme.colors.secondary};
+    }
+  }
+`
